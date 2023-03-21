b8e87e9202eaaa3c7090435faacee80c209cbd16
Tanish Sharma <tanishsharma.krish@gmail.com>
2023-03-20 21:50:28 -0400

    post hackathon

--- AItalk.py
+++ AItalk.py
@@ -7,31 +7,24 @@ def report():
     openai.api_key = "KEY"
     filename = 'output.wav'
     audio.record_audio()
-    # text = speechToText.speechToText(filename)     
-    # print(text) 
-    # messages = [{"role": "system", "content": "Assume you are working as emergency liner. Summarize the following information, extract the address and situation as bullet point (if it is ridiculous, returns: FAKE REPORT;):"}]    
-    # messages.append({"role": "user", "content": text},)    
-    # chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)    
-    # text = str(chat.choices[0].message.content)   
-    # print(text) 
-    return """
-    Address:
-    - 350 Victoria St
-
-    Situation:
-    - Man armed with gun
-    - Threatening to kill people
-    """
+    text = speechToText.speechToText(filename)     
+    print(text) 
+    messages = [{"role": "system", "content": "Assume you are working as emergency liner. Summarize the following information, extract the address and situation as bullet point (if it is ridiculous, returns: FAKE REPORT;):"}]    
+    messages.append({"role": "user", "content": text},)    
+    chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)    
+    text = str(chat.choices[0].message.content)   
+    print(text) 
+    return text
 
 def address(incident_report):
     print(incident_report)
     if "FAKE REPORT" in incident_report:
         return "Fake Report"
-    if "Not enough information" in incident_report:
-        return "Not enough information"
     relevant_text = incident_report[incident_report.find("Address")+9:incident_report.find("Situation")].replace(".", "").replace("-", "").strip()
-    return maps.extract_address_coordinates(relevant_text), maps.map_link(relevant_text)
-
+    addressUrl= maps.map_link(relevant_text)
+    address = maps.extract_address_coordinates(relevant_text)
+    print(addressUrl)
+    return address, addressUrl
 #response = inference("Hi")
-# reporttext = report()
-# print(address(reporttext))
+#reporttext = report()
+#print(address(reporttext))
--- app.py
+++ app.py
@@ -1,29 +1,57 @@
-from flask import Flask, request
-import flask
-import os
-import librosa
+from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 import AItalk
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map, icons
+#lib>[your python version]>site-packages>flask_googlemaps>templates>googlemaps>gmapjs.html and around line 46 -- fullscreenControl: blablabla , add a comma at the endf that line, save and test it.
+
+text =  AItalk.report()
+addressco, addressUrl = AItalk.address(text)
 
 
-app = Flask(__name__)
-CORS(app)
 
-@app.route('/upload')
-def hello_world():
+googleApp = Flask(__name__,template_folder='templates')
+app = Flask(__name__,template_folder='report')
+googleApp.config['GOOGLEMAPS_KEY'] = "AIzaSyD_ils9W_xYR9QAd5ZORGl8aF4pU0N4jTA"
+GoogleMaps(googleApp,key="AIzaSyD_ils9W_xYR9QAd5ZORGl8aF4pU0N4jTA")
+
+@googleApp.route('/')
+def map_created_in_view():
+
+    gmap = Map(
+        identifier="gmap",
+        varname="gmap",
+        lat=37.4419,
+        lng=-122.1419,
+        center_on_user_location=[(float(addressco[0]), float(addressco[1]))],
+        markers={
+            icons.dots.red: [(float(addressco[0]), float(addressco[1]), text)],
+        },
+        styles="height:400pixels;width:600pixels;margin:0;",
+    )
+
+    return render_template("simple.html", gmap=gmap)
+
+
+
+
+
     # Get files with wav suffix
     #files = [os.path.join("../Downloads",f) for f in os.listdir('../Downloads') if f.endswith('.wav')]
     # filepath
     #files = "output.wav"
+    
     # audioPath = files[-1]
-    text =  AItalk.report()
+    
+    #return flask.jsonify({"coordinate": addressco, "report": text}),render_template('map.html', mymap=mymap)
 
-    addressco, addressUrl = AItalk.address(text)
 
-    print(addressco)
-    print(addressUrl)
-    return flask.jsonify({"coordinate": addressco, "map": addressUrl, "report": text})
 
 
 if __name__ == '__main__':
-    app.run()
+   
+    googleApp.run(port=5050)
+    #app.run(port=5010)
+
+
+    
